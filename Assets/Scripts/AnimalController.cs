5666170b31ac00b25f4eb7f2bc7041640aa12b10
HaplessRealization <haplessrealization@gmail.com>
2026-02-24 17:29:19 -0600

    attempted to implement sfx to animals

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -4,4 +4,13 @@ public class AnimalController : MonoBehaviour
 {
     public Transform targetPoint;
     public int pointValue;
+    [SerializeField] private GameObject sfxPrefab;
+
+    private void OnEnable()
+    {
+        if(sfxPrefab != null)
+        {
+            GameObject.FindAnyObjectByType<AudioManager>().InstantiateSound(sfxPrefab, transform, true);
+        }
+    }
 }
