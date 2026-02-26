f6ec7571c501a2ed75e6ee20b05168ae99f6979c
HaplessRealization <haplessrealization@gmail.com>
2026-02-25 20:32:49 -0600

    FIXED SFX ON AWAKE

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -5,10 +5,12 @@ public class AnimalController : MonoBehaviour
     public Transform targetPoint;
     public int pointValue;
     [SerializeField] private GameObject sfxPrefab;
+    private int stupidCount = 0;
 
     private void OnEnable()
     {
-        if(sfxPrefab != null)
+        stupidCount++;
+        if(sfxPrefab != null && stupidCount == 2)
         {
             GameObject.FindAnyObjectByType<AudioManager>().InstantiateSound(sfxPrefab, transform, true);
         }
