4c521895583ee2fe519fecc033c046ec7d12c6a8
0wifi <104783815+0wifi@users.noreply.github.com>
2026-03-06 12:47:59 -0600

    small refactor

--- Assets/Scripts/FoliageController.cs
+++ Assets/Scripts/FoliageController.cs
@@ -9,13 +9,15 @@ using Random = UnityEngine.Random;
 
 public class FoliageController : MonoBehaviour
 {
-    
+
 
     [SerializeField] private List<FoliagePiece> foliagePieces;
-    [SerializeField][InfoBox("Causes foliage to spawn more often. Anything " +
-        "above 2 is probably a bad idea.")] private int foliageDensity;
-    [SerializeField][InfoBox("The range of time (in seconds) between spawns " +
-        "once each piece of foliage has left the screen.")] private Vector2 
+    //[SerializeField][InfoBox("Causes foliage to spawn more often. Anything " +
+    //"above 2 is probably a bad idea.")] private int foliageDensity;
+    [SerializeField]
+    [InfoBox("The range of time (in seconds) between spawns " +
+        "once each piece of foliage has left the screen.")]
+    private Vector2
         spawnDelay;
     [SerializeField] private float spawnXValue;
     [SerializeField] private float despawnXValue;
@@ -25,20 +27,23 @@ public class FoliageController : MonoBehaviour
     /// </summary>
     private void Start()
     {
-        for (int i = 0; i < foliageDensity; i++)
-        {
-            StartCoroutine(FoliageMover());
-        }
+
+        StartCoroutine(FoliageSpawner());
+
+    }
+
+    private IEnumerator FoliageSpawner()
+    {
+        // random delay
+        yield return new WaitForSeconds(Random.Range(spawnDelay.x, spawnDelay.y));
+        StartCoroutine(FoliageMover());
+        StartCoroutine(FoliageSpawner());
     }
 
     private IEnumerator FoliageMover()
     {
-        // delay foliage spawn 
-        yield return new WaitForSeconds(Random.Range(spawnDelay.x, spawnDelay.
-            y));
 
-        Debug.Log("Foliage spawned");
-
+        //Debug.Log("Foliage spawned");
 
         // instantiate random prefab
         FoliagePiece selectedFoliagePiece = foliagePieces[(int)Random.Range(0,
@@ -51,26 +56,26 @@ public class FoliageController : MonoBehaviour
         Vector3 foliageSpawnPosition = new Vector3
             (
             spawnXValue,
-            Random.Range(selectedFoliagePiece.spawnHeightRange.x 
-                ,selectedFoliagePiece.spawnHeightRange.y),
+            Random.Range(selectedFoliagePiece.spawnHeightRange.x
+                , selectedFoliagePiece.spawnHeightRange.y),
             currentFoliage.transform.position.z
             );
 
         currentFoliage.transform.position = foliageSpawnPosition;
 
+        //flip X half the time
+        currentFoliage.GetComponent<SpriteRenderer>().flipX = (Random.Range(0, 2) == 0);
+
         // move instance until it reaches despawn value
         while (currentFoliage.transform.position.x > despawnXValue)
         {
             yield return null;
-            currentFoliage.transform.Translate(Vector2.left * 
+            currentFoliage.transform.Translate(Vector2.left *
                 selectedFoliagePiece.moveSpeed * Time.deltaTime);
         }
 
         // delete instance
         Destroy(currentFoliage);
-
-        //restart coroutine
-        yield return StartCoroutine(FoliageMover());
     }
 
 }
@@ -81,5 +86,6 @@ public class FoliagePiece
     public GameObject prefab;
     [MinMaxSlider(-5f, 5f)] public Vector2 spawnHeightRange;
     [InfoBox("Keep equal to the speed of the background parallax for best " +
-        "results (3).")] public float moveSpeed;
+        "results (3).")]
+    public float moveSpeed;
 }
