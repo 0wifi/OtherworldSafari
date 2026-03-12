1052cd68fa0bc9ddb5480f09c60bab920326986b
0wifi <104783815+0wifi@users.noreply.github.com>
2026-03-12 16:43:12 -0500

    initial commit

--- Assets/Scripts/VisualTimer.cs
+++ Assets/Scripts/VisualTimer.cs
@@ -16,9 +16,10 @@ public class VisualTimer : MonoBehaviour
         float currentTime = 0.0f;
         while(currentTime < 120.0f)
         {
-            yield return new WaitForSeconds(1);
-            currentTime++;
-            TimerSlider.value = currentTime;
+            currentTime += Time.deltaTime;
+            TimerSlider.value = currentTime / 120.0f;
+            yield return null;
         }
     }
+
 }
