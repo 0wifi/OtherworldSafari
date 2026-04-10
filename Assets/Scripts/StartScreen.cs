fd62531462c3569ab49905a3ff450ae381d6f1ab
0wifi <104783815+0wifi@users.noreply.github.com>
2026-04-10 14:52:36 -0500

    "final" fix

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -16,6 +16,7 @@ public class StartScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerInput = FindAnyObjectByType<PlayerInput>();
         motorManager = FindAnyObjectByType<MotorManager>();
 
         playerInput.currentActionMap.Enable();
